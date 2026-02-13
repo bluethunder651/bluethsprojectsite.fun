d6aa4374ab6cf1da85534ce932aaea911fe565bf
bluethunder651 <60164112+bluethunder651@users.noreply.github.com>
2026-02-13 05:36:07 -0800

    Update songDatabase with new songs
    Added new songs to the easy and medium categories in the song database.

--- songs.js
+++ songs.js
@@ -2,10 +2,29 @@ const songDatabase = {
   easy: [
       {
         title: "Making All Things New",
-        artist: "Aaron Espe",
-        startTime: 30,
-        uri: "spotify:track:6VwAh8Z1d5YKoSWoEaV4db",
-        youtubeId: "gIqRaoLWaSo"
+        artist: "Aaron Espe"
+      },
+      {
+        title: "Washing Machine Heart",
+        artist: "Mitski"
       }
-    ]
+    ],
+  medium: [
+    {
+      title: "Here Comes The Sun - Remastered 2009",
+      artist: "The Beatles"
+    }
+  ],
+  hard: [
+    {
+      title: "Skyfall",
+      artist: "Adele"
+    }
+  ],
+  expert: [
+    {
+      title: "Sympathy For The Devil",
+      artist: "The Rolling Stones"
+    }
+  ]
 }
