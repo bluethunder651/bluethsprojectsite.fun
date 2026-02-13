f3189adec718a3e96f523570fe591dcd218d967b
bluethunder651 <60164112+bluethunder651@users.noreply.github.com>
2026-02-13 04:44:16 -0800

    Initialize song database with one entry
    Add a song database with an easy category.

--- songs.js
+++ songs.js
@@ -1,1 +1,11 @@
-
+const songDatabase = {
+  easy: [
+      {
+        title: "Making All Things New",
+        artist: "Aaron Espe",
+        startTime: 30,
+        uri: "spotify:track:6VwAh8Z1d5YKoSWoEaV4db".
+        youtubeId: "gIqRaoLWaSo"
+      }
+    ]
+}
