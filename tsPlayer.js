a069144ee34bbffec308771bf08a5c8a4f44cfd7
bluethunder651 <60164112+bluethunder651@users.noreply.github.com>
2026-02-15 04:04:49 -0800

    Add files via upload

--- tsPlayer.js
+++ tsPlayer.js
@@ -1,6 +1,6 @@
 class tsPlayer{
     constructor(){
-        this.serverUrl = 'https://julia-server.duckdns.org:5000';
+        this.serverUrl = 'https://julia-server.duckdns.org:25565';
         this.token = null;
         this.tokenExpiry = null;
         this.statusCallbacks = [];
