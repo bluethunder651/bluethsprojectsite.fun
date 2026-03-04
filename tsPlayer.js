2a725c79e1bd2b4654c98be06d520e677e015d67
bluethunder651 <60164112+bluethunder651@users.noreply.github.com>
2026-03-03 22:12:15 -0800

    Add files via upload

--- tsPlayer.js
+++ tsPlayer.js
@@ -1245,7 +1245,7 @@ class tsPlayer{
             container.appendChild(div);
         });
 
-        document.querySelectorAll('.tristate').onclick = this.tristateHandler;
+        document.querySelector('.tristate').onclick = this.tristateHandler;
     }
 
     toggleSection(header){
