a5b17b6ded86fb03209c7f8258a693f9d14a55b0
bluethunder651 <60164112+bluethunder651@users.noreply.github.com>
2026-03-31 01:25:43 -0700

    Add files via upload

--- tsGame2.js
+++ tsGame2.js
@@ -799,8 +799,7 @@ class SingleplayerGame{
             document.getElementById('game-screen').style.display = 'none';
             document.getElementById('loading-screen').style.display = 'block';
 
-            await this.prepareNextRound();
-            await this.startPlayback();
+            await this.prepareNextRound().then(result => {this.startPlayback()});
         }
     }
 
