9a8bbe3a0eee1c3c6b0abd21980e9e8103595604
bluethunder651 <60164112+bluethunder651@users.noreply.github.com>
2026-04-07 01:29:49 -0700

    Add files via upload

--- musicGame.js
+++ musicGame.js
@@ -989,7 +989,7 @@ class MusicQuizGame{
             if(response.ok){
                 const data = await response.json();
                 console.log("Data: ", data);
-                const videos = data.allVideos;
+                let videos = data.allVideos;
 
                 videos = Array.from(videos);
                 
