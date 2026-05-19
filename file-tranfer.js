669f6b5432fc500f1f3d976fddf7f25945b70428
bluethunder651 <60164112+bluethunder651@users.noreply.github.com>
2026-05-19 16:05:23 -0700

    Add files via upload

---
+++ file-tranfer.js
@@ -0,0 +1,17 @@
+class FileTransfer{
+    constructor(){
+        this.website = 'https://julia.bluethsprojectsite.fun';
+        this.token = null;
+    }
+
+    setupEventListeners() {
+        document.addEventListener('DOMContentLoaded', async function() {
+            const adminResponse = await fetch(`${this.website}/api/auth/verify-admin`);
+
+            if (adminResponse.ok){
+                const data = await adminResponse.json();
+                console.log('Admin: ', data.admin);
+            }
+        });
+    }
+}
